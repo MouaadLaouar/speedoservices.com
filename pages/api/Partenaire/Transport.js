0d8b406a6b04b6ce305ae2b6e1b51d2103540874
MouaadLaouar <laouarmouaad@gmail.com>
2023-04-12 16:47:23 +0100

    HELLO2

--- pages/api/Partenaire/Transport.js
+++ pages/api/Partenaire/Transport.js
@@ -7,7 +7,7 @@ import Transport from '@/Emails/Partenaire/Transport';
 
 const upload = multer({
     storage: multer.diskStorage({
-      destination: './public/uploads',
+      destination: './tmp',
       filename: (req, file, cb) => cb(null, file.originalname),
     }),
 });
